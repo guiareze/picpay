59f6505c3abfbf0185566ffdeaf0d90911c00154
Guilherme Rezende <guilhermearesende1@gmail.com>
2025-10-10 18:04:07 -0300

    feat: first commit with mysql local (docker)

---
+++ src/main/java/br/com/guiareze/picpay/PicpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.guiareze.picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PicpayApplication.class, args);
+	}
+
+}
