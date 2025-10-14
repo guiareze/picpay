59eff0cb2db75e4a2e7805f8906f20e74395f919
Guilherme Rezende <guilhermearesende1@gmail.com>
2025-10-14 14:50:20 -0300

    feat: updates logs on persistence services

--- src/main/java/br/com/guiareze/picpay/persistence/adapter/AccountRepositoryImpl.java
+++ src/main/java/br/com/guiareze/picpay/persistence/adapter/AccountRepositoryImpl.java
@@ -7,10 +7,12 @@ import br.com.guiareze.picpay.persistence.entity.AccountEntity;
 import br.com.guiareze.picpay.persistence.exception.PersistenceException;
 import br.com.guiareze.picpay.persistence.repository.AccountJpaRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class AccountRepositoryImpl implements AccountRepository {
@@ -19,37 +21,46 @@ public class AccountRepositoryImpl implements AccountRepository {
 
     @Override
     public Account findById(Long id) {
+        log.info("Finding account by id: {}", id);
+
         Optional<AccountEntity> entityFound;
         try {
             entityFound = repository.findById(id);
         } catch (Exception exception){
             throw new PersistenceException("Error to find account by account id", exception);
         }
+
         return entityFound.map(AccountMapper::toDomain)
                 .orElseThrow(()  -> new PersistenceException("Account not found"));
     }
 
     @Override
     public Account findByAccountIdAndUserId(Account account) {
+        log.info("Finding account by account id: {} and user id: {}", account.getId(), account.getUserId());
+
         Optional<AccountEntity> entityFound;
         try {
             entityFound = repository.findByIdAndUserId(account.getId(), account.getUserId());
         } catch (Exception exception){
             throw new PersistenceException("Error to find account by account id / user id", exception);
         }
+
         return entityFound.map(AccountMapper::toDomain)
                 .orElseThrow(()  -> new PersistenceException("Account not found"));
     }
 
     @Override
     public Account save(Account account) {
+        log.info("Saving account: {}", account);
         AccountEntity entityEntry = AccountMapper.toEntity(account);
+
         AccountEntity entitySaved;
         try {
             entitySaved = repository.save(entityEntry);
         } catch (Exception exception){
             throw new PersistenceException("Error to save account", exception);
         }
+
         return AccountMapper.toDomain(entitySaved);
     }
 }
--- src/main/java/br/com/guiareze/picpay/persistence/adapter/UserRepositoryImpl.java
+++ src/main/java/br/com/guiareze/picpay/persistence/adapter/UserRepositoryImpl.java
@@ -7,10 +7,12 @@ import br.com.guiareze.picpay.persistence.entity.UserEntity;
 import br.com.guiareze.picpay.persistence.exception.PersistenceException;
 import br.com.guiareze.picpay.persistence.repository.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
@@ -19,24 +21,30 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User save(User user) {
+        log.info("Saving user: {}", user);
         UserEntity entityToSave = UserMapper.toEntity(user);
+
         UserEntity entitySaved;
         try{
             entitySaved = repository.save(entityToSave);
         } catch (Exception exception) {
             throw new PersistenceException("Error to save user", exception);
         }
+
         return UserMapper.toDomain(entitySaved);
     }
 
     @Override
     public User findById(Long id) {
+        log.info("Finding user by id: {}", id);
+
         Optional<UserEntity> entity;
         try {
             entity = repository.findById(id);
         } catch (Exception exception) {
             throw new PersistenceException("Error to find user by id", exception);
         }
+
         return entity.map(UserMapper::toDomain)
                 .orElseThrow(() -> new PersistenceException("User not found"));
     }
